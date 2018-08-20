96ff09d08f03fd1f0cde97467c56798d07086670
Zongmin Lei <leizongmin@gmail.com>
2018-08-20 16:55:54 +0800

    cmd: 支持安装为systemd服务

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # tora
 
 运维部署系统，包括文件传输、命令执行、日志监控等模块
+
+## 使用方法
+
+```bash
+# 单独启动服务
+tora-server -c config.yaml
+
+# 单独启动服务，如果配置文件不存在，则创建默认配置文件
+tora-server -c config.yaml -init
+```
+
+安装为 systemd 服务：
+
+```bash
+# 安装为系统服务（systemd）
+tora-server -install -c config.yaml
+
+# 删除已安装的系统服务（systemd）
+tora-server -uninstall
+
+# 启动服务
+sudo systemctl start tora.service
+
+# 停止服务
+sudo systemctl stop tora.service
+```
+
+## License
+
+GPLv3
--- cmd/tora-server/main.go
+++ cmd/tora-server/main.go
@@ -5,24 +5,40 @@ import (
 	"fmt"
 	"github.com/leizongmin/tora/server"
 	"github.com/sirupsen/logrus"
+	"io/ioutil"
 	"os"
+	"path/filepath"
+	"strings"
 )
 
 const DefaultConfigFilePath = "/etc/tora.yaml"
+const DefaultInstallType = "systemd"
+const SystemdServiceFilePath = "/lib/systemd/system/tora.service"
 
 func main() {
 
 	var configFile string
 	var init bool
+	var install bool
+	var uninstall bool
+	var installType string
 	log := logrus.New()
 
 	// 解析命令行参数
 	cmd := flag.NewFlagSet("tora-server", flag.ExitOnError)
+
 	cmd.StringVar(&configFile, "c", DefaultConfigFilePath, "set c file path")
 	cmd.BoolVar(&init, "init", false, "generate example config file")
+
+	cmd.BoolVar(&install, "install", false, "install system service")
+	cmd.BoolVar(&uninstall, "uninstall", false, "uninstall system service")
+	cmd.StringVar(&installType, "t", DefaultInstallType, "install type, you can choose: systemd")
+
 	cmd.Usage = func() {
 		fmt.Fprintf(os.Stderr, fmt.Sprintf("tora/%s\n", server.Version))
-		fmt.Fprintf(os.Stderr, "Usage: tora-server [-c filename]\n\n")
+		fmt.Fprintf(os.Stderr, "Usage: tora-server [-c filename] [-init]\n\n")
+		fmt.Fprintf(os.Stderr, "       tora-server -install [-t systemd] [-c filename]\n\n")
+		fmt.Fprintf(os.Stderr, "       tora-server -uninstall [-t systemd] [-c filename]\n\n")
 		fmt.Fprintf(os.Stderr, "Options:\n")
 		cmd.PrintDefaults()
 	}
@@ -31,6 +47,7 @@ func main() {
 	// 读取配置文件
 	c, err := LoadConfigFile(configFile)
 	if err != nil {
+		// 如果文件不存在且指定了 -init 选项则创建默认文件
 		if os.IsNotExist(err) && init {
 			c, err = CreateExampleConfigFile(configFile)
 			if err != nil {
@@ -58,6 +75,17 @@ func main() {
 		log.Errorf("Invalid log format: %s", c.Log.Format)
 	}
 
+	// 安装为系统服务
+	if install {
+		installService(log, configFile, &c, installType)
+		return
+	}
+	if uninstall {
+		uninstallService(log, configFile, &c, installType)
+		return
+	}
+
+	// 创建服务器
 	s, err := server.NewServer(server.Options{
 		Log:    log,
 		Addr:   c.Listen,
@@ -93,4 +121,55 @@ func mapConfigAuthItemToServerAuthItem(m map[string]ConfigAuthItem) (r map[strin
 		}
 	}
 	return r
+}
+
+func installService(log *logrus.Logger, configFile string, config *Config, installType string) {
+	if installType != "systemd" {
+		log.Fatalf("Unsupported service install type: %s", installType)
+	}
+	execPath, err := getExecutable()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	configFile, err = filepath.Abs(configFile)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	tpl := strings.TrimSpace(`
+[Unit]
+Description=tora-server
+
+[Service]
+Type=forking
+ExecStart=%s -c %s
+
+[Install]
+WantedBy=multi-user.target
+	`)
+	code := fmt.Sprintf(tpl, execPath, configFile)
+	err = ioutil.WriteFile(SystemdServiceFilePath, []byte(code), 0644)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Infof("Created service file: %s", SystemdServiceFilePath)
+}
+
+func uninstallService(log *logrus.Logger, configFile string, config *Config, installType string) {
+	if installType != "systemd" {
+		log.Fatalf("Unsupported service install type: %s", installType)
+	}
+	err := os.Remove(SystemdServiceFilePath)
+	if err != nil && !os.IsNotExist(err) {
+		log.Fatalln(err)
+	}
+	log.Infof("Deleted service file: %s", SystemdServiceFilePath)
+}
+
+func getExecutable() (string, error) {
+	bin, err := os.Executable()
+	if err != nil {
+		return bin, err
+	}
+	return filepath.EvalSymlinks(bin)
 }
