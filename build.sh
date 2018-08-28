81164f2377a092404ee0c514e793253bdc663485
Zongmin Lei <leizongmin@gmail.com>
2018-08-28 18:31:00 +0800

    chore: 更新build.sh

--- build.sh
+++ build.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
 build_server() {
+    CMD_NAME=tora-server
     PACKAGE=./cmd/tora-server
-    RELEASE_PATH=release/tora-server
+    RELEASE_PATH=release/
 
-    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH-darwin $PACKAGE
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH-linux $PACKAGE
-    CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH-freebsd $PACKAGE
+    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH/darwin/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH/linux/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH/freebsd/$CMD_NAME $PACKAGE
 }
 
 build_cli() {
+    CMD_NAME=tora-cli
     PACKAGE=./cmd/tora-cli
-    RELEASE_PATH=release/tora-cli
+    RELEASE_PATH=release/
 
-    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH-darwin $PACKAGE
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH-linux $PACKAGE
-    CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH-freebsd $PACKAGE
+    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH/darwin/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH/linux/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH/freebsd/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $RELEASE_PATH/windows/$CMD_NAME $PACKAGE
 }
 
 rm -rf release/*
 build_server
 build_cli
-ls -al ./release
+tree -al ./release
