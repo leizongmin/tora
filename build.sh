a7eea6711e541123bbd9c24486cfb86963060d54
Zongmin Lei <leizongmin@gmail.com>
2018-08-29 14:24:09 +0800

    chore: 更新build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
+export VERSION=unknown
+export TAG=$(git describe $(git rev-list --tags --max-count=1))
+if  [ -n "$1" ] ;then
+  export VERSION=$1
+else
+  export VERSION=$TAG
+fi
+
 build_server() {
     CMD_NAME=tora-server
     PACKAGE=./cmd/tora-server
     RELEASE_PATH=release/
 
-    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH/darwin/$CMD_NAME $PACKAGE
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH/linux/$CMD_NAME $PACKAGE
-    CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH/freebsd/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH/darwin-${VERSION}-x64/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH/linux-${VERSION}-x64/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH/freebsd-${VERSION}-x64/$CMD_NAME $PACKAGE
 }
 
 build_cli() {
@@ -15,13 +23,24 @@ build_cli() {
     PACKAGE=./cmd/tora-cli
     RELEASE_PATH=release/
 
-    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH/darwin/$CMD_NAME $PACKAGE
-    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH/linux/$CMD_NAME $PACKAGE
-    CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH/freebsd/$CMD_NAME $PACKAGE
-    CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $RELEASE_PATH/windows/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o $RELEASE_PATH/darwin-${VERSION}-x64/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o $RELEASE_PATH/linux-${VERSION}-x64/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=freebsd GOARCH=amd64 go build -o $RELEASE_PATH/freebsd-${VERSION}-x64/$CMD_NAME $PACKAGE
+    CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o $RELEASE_PATH/windows-${VERSION}-x64/$CMD_NAME $PACKAGE
 }
 
+zip_pack() {
+    cd release/$1
+    zip -r ../$1.zip *
+    cd ../..
+}
+
+echo "build version: ${VERSION}"
 rm -rf release/*
 build_server
 build_cli
+zip_pack darwin-${VERSION}-x64
+zip_pack linux-${VERSION}-x64
+zip_pack freebsd-${VERSION}-x64
+zip_pack windows-${VERSION}-x64
 tree -al ./release
